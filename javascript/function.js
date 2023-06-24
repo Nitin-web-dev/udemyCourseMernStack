c8208a1be95583b1578a698959767ce5934c8e52
Nitin Singh <Nitinwebdev5@gmail.com>
2023-06-24 22:37:27 +0530

    addead function perameter and arguments

---
+++ javascript/function.js
@@ -0,0 +1,11 @@
+/// to make some bllcok of code 
+// or do dry thing 
+
+
+function fun(usr) {  // (perameter )
+    let res = usr.toUpperCase();
+    console.log(res);
+
+}
+
+fun('sam') // (arguments)
