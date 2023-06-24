320e35a8f02e8f624d8aed586a472c40d295e73f
Nitin Singh <Nitinwebdev5@gmail.com>
2023-06-24 22:31:10 +0530

    addead slice touppercase oparoter

---
+++ javascript/someStirngMethod.js
@@ -0,0 +1,31 @@
+// // slice 
+// // let username = "Samsingh";
+// // console.log(username.slice(0,5));
+
+
+
+// // /// exercise 
+// // // write the 10 character and only print the 0-7 char 
+// // let pera = 'this is another line for text it is just ramdom';
+
+// // let validInput = pera.slice(0,7);
+// // console.log(validInput)
+
+
+
+// // to uppercase 
+// // change the text to uppercase
+
+// let usrname = 'samsingh';
+// let uppercaseUsername = usrname.toUpperCase();
+// console.log(uppercaseUsername);
+
+
+/// in js we have some arithmetic operatore
+// - + / * ** % 
+
+// increament and decreament 
+// ++ -- 
+
+
+
