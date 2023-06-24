863ae8a04dd52affef6094b46ca28a98fa5269b9
Nitin Singh <Nitinwebdev5@gmail.com>
2023-06-24 22:02:08 +0530

    addead js basic topic alert prompt variable datatypes and an exercise

---
+++ javascript/valueSwap.js
@@ -0,0 +1,35 @@
+// Javascript Variables Exercise
+// Given the existing code below, can you write some code so that their values are switched around?
+
+var a = "3";
+var b = "8";
+// So that the variable a holds the value "8".
+
+// And the variable b holds the value "3".
+
+// When the code is run, it should output:
+
+// a is 8
+
+// b is 3
+
+// Do NOT change any of the existing code.
+
+// You are NOT allowed to type any numbers.
+
+// You should NOT redeclare the variables a and b.
+
+
+
+// to solve this problem 
+// let another variable 'c'  and assign its value a;
+let c = a;  // here c is now have value of a which is  = 3;
+
+// then we change a  value to b which is  = 8;
+a = b; // means b= 8;
+
+// then just simply with b
+b= c;
+
+
+console.log(a,b)
