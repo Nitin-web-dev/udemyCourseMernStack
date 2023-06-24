fba49a8a5ebd6181cff874defdd3937485febd3a
Nitin Singh <Nitinwebdev5@gmail.com>
2023-06-24 23:43:05 +0530

    jquery

---
+++ jquery/linkJquerywithcdn/index.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $('h1').addClass('bigTitle ml'); // we can have multi classs
+    // to remove class
+    // $('h1').removeClass('bigTitle'); 
+    
+    // to check if class has or ot 
+    // $('h1').hasClass('bigTitle'); // it return boolean
+
+    $('button').text('<em>hello </em>');
+    $('button').html('<em>hello </em>');
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// search for this topics 
+
+
+//select 
+// css 
+// add remove has class multi class
+// html text val 
+// attributes   like  = src, href, att
+    // get and set 
+        // attr 
+// add event listener 
+    // (h1).click(() => {
+    //     ...code ...
+    // })
+
+    // keypress
+    //on 
+
+// adding and removing  element 
+// before and after method
+//  prepend and append method 
+
