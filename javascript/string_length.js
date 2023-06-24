be4c60a6364b135809fe1d2d3f1df88bd9323c71
Nitin Singh <Nitinwebdev5@gmail.com>
2023-06-24 22:17:05 +0530

    addead some changes

---
+++ javascript/string_length.js
@@ -0,0 +1,14 @@
+// get the string length of char
+
+
+let userName = "John doe";
+console.log(userName.length);
+
+
+// also for pera
+
+
+let para = "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur expedita earum vero excepturi eaque, voluptas facilis nam. Aliquam dolorum ipsa recusandae unde blanditiis quam culpa aspernatur tenetur hic? Cum, commodi!";
+let peraLenght = para.length;
+let res = `You have written ${peraLenght} character`;
+
