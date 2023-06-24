e6b820dfcfc5ef359e9dc06f370e7d800711af26
Nitin Singh <Nitinwebdev5@gmail.com>
2023-06-24 22:07:07 +0530

    adddead naming convention

---
+++ javascript/naming_convention.js
@@ -0,0 +1,12 @@
+// names and naming convetions for js variables
+
+// always give variable meaningful names 
+// related to its value or function
+
+// not use kwyword for names
+
+
+// read w3schools for for naming conventions 
+
+
+// let getUserInput = 'sfsfdsdf';
