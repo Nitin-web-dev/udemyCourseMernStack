12ccb7d5084b2999ad040c71747f23fb06e8f14c
Nitin Singh <Nitinwebdev5@gmail.com>
2023-06-25 12:32:13 +0530

    node learning

---
+++ node/notes.js
@@ -0,0 +1,46 @@
+// what is nodejs
+    // - node is a runtime environment for javascript which allow us to write js in computer 
+    // - its asynchronous event-driven javascript runtime, nodejs is designed to build scalable network application
+    // - it works on v8 engine
+    // - it helps in 
+        // - js fullstack
+        // - scales
+        // - non-blocking
+        // - ecosystem (npm)
+
+// why use framwork
+    // - to help some functioning predefined 
+    // - don't have to write everything from scratch
+        // - reuse components 
+        // - reduce overhead
+            // - reading files
+            
+    // - reading fiels
+    // - networking
+    // - debugging
+    // - data streams
+    // - writing files
+    // - error codes
+    // - url string
+    // - diagnostics
+    // - tests
+
+
+
+// install nodejs
+    // search and download
+
+    // using node
+        // check version - in cmd trype = node -v (to checkk version)
+
+    // node repl (read, eval, print, loop)
+        // - is a computer environment where user inputs are read and evaluated and then return 
+
+        // cmd - node (enter--)
+
+        // cmd - to exits 
+            // exit (enter--)
+            // or ctrl + c
+        
+        // run a file in js
+            // cmd - node index.js (enter--)
