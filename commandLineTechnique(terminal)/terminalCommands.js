1723806b5272dae21ff6e3f83211b0e80f9d7c41
Nitin Singh <Nitinwebdev5@gmail.com>
2023-06-25 11:48:04 +0530

    added terminal commands which regularly use

---
+++ commandLineTechnique(terminal)/terminalCommands.js
@@ -0,0 +1,16 @@
+// pwd =current working directory
+
+// ls =to see content of folder
+
+// ls -al =  for detail in file defination or formate
+// ls -a = for hidden files
+// clear = to clear the terminal
+// cd = to change direcotry 
+// cd .. = to go back into previous direcotry
+// cd - =to previous direcotry you wherre was
+// cd = to go root direcotry
+// mkdir folderNmae =  to create a folder 
+// touch filename = to make a file 
+// cp fileanme.txt = folder/filename.ctst = to copy a file and paste
+// rm = to remove a file 
+// rm -r to remove folder with file 
